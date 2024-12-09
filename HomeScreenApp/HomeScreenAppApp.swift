489ffe94373295026a2cf3265fd59fa0c02b45ca
Neeshu Kumar <neeshu.kumar@simplyentertainingapps.com>
2024-12-09 11:52:34 +0530

    Initial Commit

---
+++ HomeScreenApp/HomeScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeScreenAppApp.swift
+//  HomeScreenApp
+//
+//  Created by Neeshu Kumar on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
